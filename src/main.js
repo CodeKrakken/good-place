67a1056155f4f3a66f94e86669cd3e8d27daa171
CodeKrakken <newholm1@hotmail.com>
2020-10-17 16:40:10 +0100

    Set up state

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { auth } from './firebase'
+import './assets/scss/app.scss'
 
 Vue.config.productionTip = false
 
