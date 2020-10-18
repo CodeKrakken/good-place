4ebfb46f6e758692e403ea4cc1f2e9a0eab8c1eb
CodeKrakken <newholm1@hotmail.com>
2020-10-18 13:17:39 +0100

    See full post

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -26,7 +26,7 @@
             <ul>
               <li ><a @click="toggleCommentModal(post)">comments {{ post.comments }}</a></li>
               <li><a @click="likePost(post.id, post.likes)">likes {{ post.likes }}</a></li>
-              <li><a>view full post</a></li>
+              <li><a @click="viewPost(post)">view full post</a></li>
             </ul>
           </div>
         </div>
@@ -35,6 +35,30 @@
         </div>
       </div>
     </section>
+
+    <transition name="fade">
+      <div v-if="showPostModal" class="p-modal">
+        <div class="p-container">
+          <a @click="closePostModal()" class="close">close</a>
+          <div class="post">
+            <h5>{{ fullPost.userName }}</h5>
+            <span>{{ fullPost.createdOn | formatDate }}</span>
+            <p>{{ fullPost.content }}</p>
+            <ul>
+              <li><a>comments {{ fullPost.comments }}</a></li>
+              <li><a>likes {{ fullPost.likes }}</a></li>
+            </ul>
+          </div>
+          <div v-show="postComments.length" class="comments">
+            <div v-for="comment in postComments" :key="comment.id" class="comment">
+              <p>{{ comment.userName }}</p>
+              <span>{{ comment.createdOn | formatDate }}</span>
+              <p>{{ comment.content }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -42,6 +66,7 @@
 import { mapState } from 'vuex'
 import moment from 'moment'
 import CommentModal from '@/components/CommentModal'
+import { commentsCollection } from '@/firebase'
 
 export default {
   components: {
@@ -53,7 +78,9 @@ export default {
         content: ''
       },
       fullPost: {},
-      showCommentModal: false
+      showCommentModal: false, 
+      showPostModal: false,
+      postComments: []
     }
   },
   computed: {
@@ -75,6 +102,22 @@ export default {
     }, 
     likePost(id, likesCount) {
       this.$store.dispatch('likePost', { id, likesCount })
+    }, 
+    async viewPost(post) {
+      const docs = await commentsCollection.where('postId', '==', post.id).get()
+
+      docs.forEach(doc => {
+        let comment = doc.data()
+        comment.id = doc.id
+        this.postComments.push(comment)
+      })
+
+      this.fullPost = post
+      this.showPostModal = true
+    },
+    closePostModal() {
+      this.postComments = []
+      this.showPostModal = false
     }
   }, 
   filters: {
