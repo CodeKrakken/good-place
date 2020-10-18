cdf536b7b25089b19eb53295b6a1f34d458d695a
CodeKrakken <newholm1@hotmail.com>
2020-10-18 15:41:42 +0100

    Update user profile

--- src/store/index.js
+++ src/store/index.js
@@ -88,6 +88,32 @@ const store = new Vuex.Store({
         likes: post.likesCount + 1
       })
     },
+    async updateProfile({ dispatch }, user) {
+      const userId = fb.auth.currentUser.uid
+      // update user object
+      await fb.usersCollection.doc(userId).update({
+        name: user.name,
+        title: user.title
+      })
+    
+      dispatch('fetchUserProfile', { uid: userId })
+    
+      // update all posts by user
+      const postDocs = await fb.postsCollection.where('userId', '==', userId).get()
+      postDocs.forEach(doc => {
+        fb.postsCollection.doc(doc.id).update({
+          userName: user.name
+        })
+      })
+    
+      // update all comments by user
+      const commentDocs = await fb.commentsCollection.where('userId', '==', userId).get()
+      commentDocs.forEach(doc => {
+        fb.commentsCollection.doc(doc.id).update({
+          userName: user.name
+        })
+      })
+    }
   },
   modules: {
   }
